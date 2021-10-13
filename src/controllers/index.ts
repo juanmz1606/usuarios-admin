34dc027eeec0d856553def7f1fda91dbb5df0149
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2021-10-13 11:35:25 -0500

    controladores

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,6 @@ export * from './usuario-rol.controller';
 export * from './rol-usuario.controller';
 export * from './rol-permiso.controller';
 export * from './permiso-rol.controller';
+export * from './rol.controller';
+export * from './usuario.controller';
+export * from './permiso.controller';
